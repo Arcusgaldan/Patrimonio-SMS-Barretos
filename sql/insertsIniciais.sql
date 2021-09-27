df513a5201eb3eca286c0b0928c30c4aeb99079d
Arcusgaldan <thales_de_mattos@hotmail.com>
2021-09-27 09:58:46 -0300

    Organização SQL

---
+++ sql/insertsIniciais.sql
@@ -0,0 +1,2 @@
+INSERT INTO TBUsuario VALUES (0, "Administrador", "admin@email.com", "bd7822c76f3ca74cd699115b8128cbcb2c908ae53f2078bce4b5abd901401818", 0);
+INSERT INTO TBTipoItem (id, nome) VALUES (1, "PC");
